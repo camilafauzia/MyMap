1f673de6ffd7f4288a007335f14967e9933a2347
camilafauzia <camilafauzia@mail.ugm.ac.id>
2024-09-16 13:22:14 +0700

    Acara 4

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
   constructor() {}
+  private latitude: number | any;
+  private longitude: number | any;
+
+  public async ngOnInit() {
+    const position = await Geolocation.getCurrentPosition();
+    this.latitude = position.coords.latitude;
+    this.longitude = position.coords.longitude;
+    // this.longitude = 106.86008819340901;
+    // this.latitude = -6.125417449443821;
+    const map = new Map({
+      basemap: "topo-vector"
+    });
+    const view = new MapView({
+      container: "container",
+      map: map,
+      zoom: 14,
+      center: [this.longitude, this.latitude]
+    });
+  }
 
 }
